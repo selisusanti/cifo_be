6ff89701144a120644d65b1cb36fb060c4a91896
selisusanti <seli.susanti@indosystem.com>
2022-06-30 13:57:49 +0700

    update baru

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,15 +8,19 @@ use App\User;
 use Validator;
 use App\Services\Response;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Helpers\Paginator;
 
 class UserController extends Controller
 {
-    public function index(){
-        $listUser                   = User::get();
-        $response['status']         = true;
-        $response['message']        = 'Berhasil registrasi';
-        $response['data']           = $listUser;
-        return response()->json($response, 200);
+    public function index(Request $request){
+        $listUser       = User::latest()->get();
+        $page           = $request->page ? $request->page : 1 ;
+        $perPage        = $request->limit ?? 10;
+        $ALL_listUser   = collect($listUser);
+        $dispatcher_new = new Paginator($ALL_listUser->forPage($page, $perPage), $ALL_listUser->count(), $perPage, $page, [
+            'path' => url("api/user")
+        ]); 
+        return Response::success($dispatcher_new);
     }
 
     public function userProfile(){
